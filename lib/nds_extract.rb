879c51d6a61477628a1cc4c51f47fca7804ec770
Reese M Bowman <reesemb1@gmail.com>
2019-12-26 20:19:41 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,17 +21,5 @@ return results
 end
 
 
-  director_index = 0 
-  while director_index < nds.length do
-    director_name = nds[director_index][:name]
-    movie_index = 0
-    movie_gross = 0 
-    while movie_index < nds[director_index][:movies].length do
-      movie_gross += nds[director_index][:movies][movie_index][:worldwide_gross]
-      movie_index += 1
-    end
-    result[director_name] = movie_gross
-    director_index += 1
-  end
-  return result
+
 end
